c80807d620e3831050da1286e303d2567e7ec989
VladKindrativ <kindrativvlad62@gmail.com>
2024-10-22 21:48:23 +0300

    Add files via upload

---
+++ Casino.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "CasinoMashine.h"
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	int balance = 1000;
+	CasinoMashine Machine;
+	CasinoWheel MachineWhell;
+
+	// I haven't promoted the teams yet :) 
+	
+	
+} 
+ 
---
+++ CasinoMashine.cpp
@@ -0,0 +1,1 @@
+#include "CasinoMashine.h"
